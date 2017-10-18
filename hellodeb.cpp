a8bb3e43c3caadb631b8a10e71979329ee9a3973
Sk Ahmad <ahmad.sk98@gmail.com>
2017-10-18 22:42:34 +0530

    It is source file for creating my application.
    after compiling this source file we will have get a file which we install in our system by debian package.

---
+++ hellodeb.cpp
@@ -0,0 +1,20 @@
+/*
+  file Name : hellodeb.cpp
+  date : 18/10/2017
+  author : sk ahamd
+  description : linux debian package tutorial.
+  
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout<<"Hello world"<<endl;
+  cout<<"In this tutorial I am creating a simple linux debian packge."<<endl;
+  cout<<"Thank You!"<<endl;
+  return 1;
+  
+ }
+ 
